fe06e7f40005e0af5cee27da04ec9f6a3a91d49a
Sergiy <s.kava1997@gmail.com>
2020-10-14 16:40:25 +0300

    Revert "finish postgres"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,10 @@
-import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { CommonModule } from './common/common.module';
-import appConfig from './config/app.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        autoLoadEntities: true,
-        synchronize: true
-      })
-    }),
-    ConfigModule.forRoot({
-     load: [appConfig]
-    }),
-    CoffeesModule,
-    CoffeeRatingModule,
-    CommonModule,
-    //DatabaseModule
-  ],
+  imports: [CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,50 +1,19 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Inject,
-  Param,
-  Patch,
-  Post,
-  Query,
-  Res, SetMetadata, UsePipes, ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
-import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
-import { Public } from '../common/decorators/public.decorator';
-import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
-import { Protocol } from '../common/decorators/protocol.decorator';
-import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
-  constructor(
-    private readonly coffeeService: CoffeesService,
-    @Inject(REQUEST) private request: Request
-  ) {
-    console.log('CoffeesController created');
-  }
-
-  @ApiForbiddenResponse({description: 'Forbidden'})
-  @Public()
-  @UsePipes(new ValidationPipe())
+  constructor(private readonly coffeeService: CoffeesService) {}
   @Get()
-  findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
-    console.log(protocol);
-    return this.coffeeService.findAll(paginationQuery);
+  findAll(@Query() paginationQuery) {
+    //const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: string) {
-    console.log(id);
+  findOne(@Param('id') id: string) {
     return this.coffeeService.findOne(id);
   }
 
@@ -55,7 +24,7 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeeService.update(id, updateCoffeeDto);
   }
 
